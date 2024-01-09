2fb5df28eccf6205b88168583fc95cb3234b2edc
damaso-pierre.de.la.cruz <damaso-pierre.de.la.cruz@estudiantat.upc.edu>
2024-01-09 02:08:58 +0100

    KNOWN SSA VALUES!!!! Now back to fixing the addition of a const AddIOp to the upperbound in case SLE SLT, you know... fun stuff! And continue with the loopTransformationMagic

--- clang/lib/CodeGen/ll/LiftingLoopsPass.cpp
+++ clang/lib/CodeGen/ll/LiftingLoopsPass.cpp
@@ -1,42 +1,84 @@
 #include "LiftingLoopsPass.h"
+#include "mlir-c/IR.h"
+#include "mlir/Analysis/CFGLoopInfo.h"
 #include "mlir/Dialect/LLVMIR/LLVMDialect.h"
+#include "mlir/Dialect/SCF/IR/SCF.h"
+#include "llvm/Support/Debug.h"
+#include "mlir/Dialect/Affine/Analysis/LoopAnalysis.h"
+#include "mlir/Dialect/SCF/Transforms/Passes.h"
+#include "mlir/IR/Block.h"
+#include "mlir/IR/FunctionInterfaces.h"
+#include "mlir/IR/OpDefinition.h"
+#include "mlir/IR/Value.h"
+
+#include "llvm/Analysis/LoopInfo.h"
+
+#include "mlir/Dialect/Affine/TransformOps/AffineTransformOps.h"
+#include "mlir/Dialect/Func/IR/FuncOps.h"
+#include "mlir/Dialect/Index/IR/IndexOps.h"
+#include "mlir/Dialect/Index/IR/IndexAttrs.h"
+#include "mlir/Dialect/Index/IR/IndexDialect.h"
+
+
+#include "mlir/Dialect/Index/IR/IndexAttrs.h"
+#include "mlir/Dialect/Index/IR/IndexDialect.h"
+#include "mlir/IR/Builders.h"
+#include "mlir/IR/Matchers.h"
+#include "mlir/IR/OpImplementation.h"
+#include "mlir/Interfaces/InferIntRangeInterface.h"
+#include "mlir/Interfaces/Utils/InferIntRangeCommon.h"
+#include "llvm/ADT/SmallString.h"
+#include "llvm/ADT/TypeSwitch.h"
+
 
 #define DEBUG_TYPE "lifting-loops"
 
 namespace ll{
 
+  
+struct MyPattern : public mlir::RewritePattern {
+  public:
+  MyPattern(mlir::MLIRContext *context)
+      : mlir::RewritePattern(mlir::scf::ForOp::getOperationName(), /*benefit=*/1, context) {}
+
+  mlir::LogicalResult
+  matchAndRewrite(mlir::Operation *op,
+                  mlir::PatternRewriter &rewriter) const override {
+    return mlir::success();
+  }
+};
+
   void LiftingLoopsPass::runOnOperation() {
     mlir::Operation *op = getOperation();
+    op->dump();
     assert(op->getNumRegions() == 1);
     mlir::Region &region = op->getRegion(0);
     
     llvm::dbgs() << " Dumping original blocks!\n";
-    for (auto &b : region.getBlocks())
-      b.dump();
+    for (mlir::Block &b : region.getBlocks())
+      //b.dump();
     llvm::dbgs() << " \n";
 	
-    // Initialize variables for loop information
     mlir::BlockArgument indVar;
     mlir::Value lb, ub, step;
+    mlir::Block *header;
 
-    // Get dominance information and construct loop information
     mlir::DominanceInfo &domInfo = getAnalysis<mlir::DominanceInfo>();
     llvm::DominatorTreeBase<mlir::Block, false> &domTree = domInfo.getDomTree(&region);
     mlir::CFGLoopInfo loopInfo(domTree);
     assert(!loopInfo.getTopLevelLoopsVector().empty());
     
+mlir::LoopLikeOpInterface loop;
     loopInfo.print(llvm::dbgs());
     llvm::dbgs() << " \n";
     
-    // Extract loop properties
     for (auto *it : loopInfo.getTopLevelLoopsVector()){
       for (auto *b : it->getBlocks()){
         if (loopInfo.isLoopHeader(b)){
-          // Extract loop induction variable and bounds
+          header = b;
           for (auto &ba : b->getArguments()){
             if (ba.isUsedOutsideOfBlock(b)){
               indVar = ba;
-              // Extract lower bound from the terminator of the predecessor block
               for (mlir::Block *predBlock : ba.getOwner()->getPredecessors()) {
                 if (loopInfo.getLoopFor(predBlock) == nullptr){
                   if (mlir::Operation *terminator = predBlock->getTerminator()) {
@@ -44,7 +86,6 @@ namespace ll{
                   }
                 }
               }
-              // Extract upper bound from the loop predicate compare
               for (auto *op : ba.getUsers()){
                 for (mlir::NamedAttribute arg : op->getAttrs()){
                   if (arg.getName() == "predicate" && arg.getValue().isa<mlir::IntegerAttr>()) {
@@ -61,7 +102,6 @@ namespace ll{
             }
           }
         }
-        // Extract step value from the loop latch block
         if (it->isLoopLatch(b)){
           for (auto &op : b->getOperations()){
             if (op.getOpOperands().size() == 2){
@@ -80,7 +120,54 @@ namespace ll{
       llvm::dbgs() << "LowerBound: " << lb << "\n";
       llvm::dbgs() << "Step: " << step << "\n";
 
+
+
+      mlir::Region &region = op->getRegion(0);
+      mlir::Block &entryBlock = region.getBlocks().front();
+      mlir::OpBuilder builder(op->getContext());
+
+auto ip = builder.saveInsertionPoint();
+builder.setInsertionPointAfter(getOperation());
+
+      auto loc = header->begin()->getLoc();
+
+      llvm::dbgs() << "preeeee:\n";
+      //op->print(llvm::dbgs(), mlir::OpPrintingFlags().printGenericOpForm().elideLargeElementsAttrs());
+      getOperation()->getRegion(0).getOps().begin()->getLoc()->dump();
+      getOperation()->getRegion(0).getOps().begin()->dump();
+
+      auto forOp = builder.create<mlir::scf::ForOp>(getOperation()->getRegion(0).getOps().begin()->getLoc(), lb, ub, step);
+      forOp->print(llvm::dbgs(), mlir::OpPrintingFlags().printGenericOpForm().elideLargeElementsAttrs());
+      builder.restoreInsertionPoint(ip);
+
+
+      
     }
+/*auto bodyBuilder = mlir::OpBuilder::atBlockEnd(forOp.getBody());
+auto iv = forOp.getInductionVar();
+auto ivType = iv.getType();
+auto ptr = bodyBuilder.create<mlir::LLVM::IntToPtrOp>(entryBlock.front().getLoc(), ivType, iv);
+auto storeOp = bodyBuilder.create<mlir::LLVM::StoreOp>(entryBlock.front().getLoc(), iv, ptr);
+
+llvm::dbgs() << "pppp\n";
+storeOp->print(llvm::dbgs(), mlir::OpPrintingFlags().printGenericOpForm().elideLargeElementsAttrs());
+
+for (auto it : llvm::zip(entryBlock.front().getResults(), forOp.getResults())) {
+std::get<0>(it).replaceAllUsesWith(std::get<1>(it));
+}
+
+  llvm::dbgs() << "eeeend\n";
+for (auto &b : region.getBlocks()){
+for (auto &oper : b.getOperations()) {
+  //oper.remove();
+}
+}
+//entryBlock.erase();
+*/
+
+    
+      llvm::dbgs() << "eeeend\n";
+
   }
 
 } // namespace ll
